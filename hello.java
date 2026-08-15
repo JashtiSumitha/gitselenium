b7027944826f51acfb0ea029167204c355996ff0
Sumitha <sumithachowdary55@gmail.com>
2026-08-15 17:15:08 +0530

    third commit

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 public class hello{
 public static void main(String [] args){
 System.out.println("Hello");
+System.out.println("Making changes to pull the code");
+System.out.println("import the project in eclipse to the created and cloned git folder");
+
 }
 }
